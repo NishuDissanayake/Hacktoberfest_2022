163096534206fe49a59386f9098c3961741321c5
Ishan-Chanuka <ishanchanuka4@gmail.com>
2022-10-05 10:56:31 +0530

    Add a solution for issue #09

---
+++ C#/Ishan-09/AreaOfTrapezoid/AreaOfTrapezoid/Program.cs
@@ -0,0 +1,16 @@
+﻿using AreaOfTrapezoid;
+
+Calculation cal = new Calculation();
+
+Console.WriteLine("Enter the lower base value of the Trapezoid : ");
+cal.lbase = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the upper base value of the Trapezoid : ");
+cal.ubase = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the height value of the Trapezoid : ");
+cal.height = double.Parse(Console.ReadLine());
+
+double area = cal.AreaOfTrapezoid();
+
+Console.WriteLine($"The area of the Trapezoid : {area}");
