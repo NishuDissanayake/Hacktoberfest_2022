965fbc574bddbb59298a698a2143253d412f084e
Mahima Hemachandra <nomad.inof@gmail.com>
2022-10-11 09:58:34 +0530

    Answer for issue 10

---
+++ C#/Mahima - 10/Reverse Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 15, 16, 17, 18 };
+
+            // Original Array
+            Console.WriteLine("Original Array= ");
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Reverse Array
+            Array.Reverse(arr);
+
+            Console.WriteLine("Reversed Array= ");
+            foreach (int j in arr)
+            {
+                Console.WriteLine(j);
+            }
+            Console.ReadLine();
+        }
+    }
+    }
